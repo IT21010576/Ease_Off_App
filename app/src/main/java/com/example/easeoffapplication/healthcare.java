1a4535153fe58a3fc806983777d6ebfe30e6603e
siyumi <it21010576@my.sliit.lk>
2022-09-14 19:15:04 +0530

    initial commit

---
+++ app/src/main/java/com/example/easeoffapplication/healthcare.java
@@ -0,0 +1,14 @@
+package com.example.easeoffapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class healthcare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healthcare);
+    }
+}
