efd06e4f59ca7ed807d41f7922a8c92f741ed80a
Yashi <it21018114@my.sliit.lk>
2022-09-18 02:24:52 +0530

    Workouts

--- app/src/main/java/com/example/easeoffapplication/Workout.java
+++ app/src/main/java/com/example/easeoffapplication/Workout.java
@@ -1,14 +1,50 @@
 package com.example.easeoffapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Workout extends AppCompatActivity {
+
+    Button fragment2btn,fragment3btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        fragment2btn=findViewById(R.id.fragment2btn);
+        fragment3btn=findViewById(R.id.fragment3btn);
+
+        fragment2btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                replaceFragment(new MyProfile());
+
+            }
+        });
+        fragment3btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                replaceFragment(new MyWorkout());
+
+            }
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
+
+
     }
 }
